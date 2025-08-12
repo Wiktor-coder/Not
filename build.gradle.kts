05a616dc971a8d00a68c297c5ba3fcd13978787d
Wiktor Chumakov <Apostal333@gmail.com>
2025-08-12 22:41:43 +0300

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    kotlin("jvm") version "2.1.21"
+    id("jacoco")
+}
+
+group = "ru.netology"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation ("org.jetbrains.kotlin:kotlin-test")
+    testImplementation ("junit:junit:4.13.2")
+}
+
